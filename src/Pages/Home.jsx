e149dffdc6fed0c9ebfcfd10eb2046113aa9b1bd
unknown <mashrafi.reja@northsouth.edu>
2025-02-18 19:39:43 +0600

    Changes to home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,3 @@
-/****************************************************************************************************************************************************************
-
-import React from 'react'
-import Hero from '../Components/Hero/Hero';
-
-const Home = () => {
-    return (
-        <div>
-            <Hero/>
-            
-            <h2>About Us</h2>
-            <h2>Contact Information</h2>
-            <p>Email: contact@website.com</p>
-            <p>Phone: (123) 456-7890</p>
-        </div>
-    );
-};
-
-export default Home;
-
-****************************************************************************************************************************************************************/
-
 import React from 'react';
 import Hero from '../Components/Hero/Hero'; // Assuming you have a Hero component
 import './Home.css'; // You can create a separate CSS file for styling
